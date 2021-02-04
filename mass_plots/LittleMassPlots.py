b3db4b566cbded8565ff634b33787167bc83acea
YasmineAmhis <amhis@Yasmine-2.home>
2021-02-04 15:22:45 +0100

    update plots

--- mass_plots/LittleMassPlots.py
+++ mass_plots/LittleMassPlots.py
@@ -17,16 +17,16 @@ from array import array
 from copy import deepcopy
 
 gStyle.SetOptStat("")
-gROOT.ProcessLine(".x lhcbStyle.C")
+gROOT.ProcessLine(".x ../utils/lhcbStyle.C")
 
-gStyle.SetOptStat("")
-ROOT.gSystem.Load("libMathMore") #to get Legendre
 
 from math import pow
 
 
 if __name__ == '__main__':
 
+
+     #Hallo little files
      fBsToKstmm = ROOT.TFile("BsToKstmm_tree.root", 'UPDATE')
      tBsToKstmm = fBsToKstmm.Get("DecayTree")
 
@@ -39,14 +39,26 @@ if __name__ == '__main__':
      fBdToKstee = ROOT.TFile("BdToKstee_tree.root", 'UPDATE')
      tBdToKstee = fBdToKstee.Get("DecayTree")
 
+     fBdToRhomm = ROOT.TFile("BdToRhomm_tree.root", 'UPDATE')
+     tBdToRhomm = fBdToRhomm.Get("DecayTree")
+
+     fBdToRhoee = ROOT.TFile("BdToRhoee_tree.root", 'UPDATE')
+     tBdToRhoee = fBdToRhoee.Get("DecayTree")
 
 
+     #Hallo little histograms
      BsKstmm_mass_hist = r.TH1F("BsKstmm_mass_hist", "BsKstmm_mass_hist", 100, 5100, 5700)
      BdKstmm_mass_hist = r.TH1F("BdKstmm_mass_hist", "BdKstmm_mass_hist", 100, 5100, 5700)
+     BdRhomm_mass_hist = r.TH1F("BdRhomm_mass_hist", "BdRhomm_mass_hist", 100, 5100, 5700)
+
      BsKstee_mass_hist = r.TH1F("BsKstee_mass_hist", "BsKstee_mass_hist", 100, 4100, 5700)
      BdKstee_mass_hist = r.TH1F("BdKstee_mass_hist", "BdKstee_mass_hist", 100, 4100, 5700)
+     BdRhoee_mass_hist = r.TH1F("BdRhoee_mass_hist", "BdRhoee_mass_hist", 100, 4100, 5700)
 
 
+
+     #Hallo little plots
+     #K* family with muons
      cMuons =  ROOT.TCanvas ("cMuons","cMuons",750,750)
      cMuons.cd()
      cMuons.SetLogy(1)
@@ -58,9 +70,8 @@ if __name__ == '__main__':
      tBsToKstmm.Draw("B_M*1000>>BsKstmm_mass_hist", "","SAME")
      BdKstmm_mass_hist.Scale(4157*1./BdKstmm_mass_hist.Integral())
      BsKstmm_mass_hist.Scale(38*1./BsKstmm_mass_hist.Integral())
-
-     cMuons.SaveAs("Kstmm.png")
-
+     cMuons.SaveAs("plots/Kstmm.png")
+     #K* family with electrons
      cElectrons =  ROOT.TCanvas ("cElectrons","cElectrons",750,750)
      cElectrons.cd()
      cElectrons.SetLogy(1)
@@ -72,4 +83,35 @@ if __name__ == '__main__':
      tBsToKstee.Draw("B_M*1000>>BsKstee_mass_hist","","SAME")
      BdKstee_mass_hist.Scale(4157*1./BdKstee_mass_hist.Integral())
      BsKstee_mass_hist.Scale(38*1./BsKstee_mass_hist.Integral())
-     cElectrons.SaveAs("Kstee.png")
+     cElectrons.SaveAs("plots/Kstee.png")
+
+
+
+
+     #rho family with muons
+     cMuonsRho =  ROOT.TCanvas ("cMuonsRho","cMuonsRho",750,750)
+     cMuonsRho.cd()
+     cMuonsRho.SetLogy(1)
+     BdRhomm_mass_hist.SetTitle("")
+     BdRhomm_mass_hist.GetXaxis().SetTitle("m(#pi^{-}#pi^{+}#mu^{+}#mu^{-})[MeV/c^{2}]")
+     BdRhomm_mass_hist.SetMarkerColor(17)
+     BdKstmm_mass_hist.SetMarkerColor(46)
+     tBdToKstmm.Draw("B_M*1000>>BdKstmm_mass_hist","","")
+     tBdToRhomm.Draw("B_M*1000>>BdRhomm_mass_hist", "","SAME")
+     BdKstmm_mass_hist.Scale(1./BdKstmm_mass_hist.Integral())
+     BdRhomm_mass_hist.Scale(1./BdRhomm_mass_hist.Integral())
+     cMuonsRho.SaveAs("plots/Rhomm.png")
+     #rho family with electrons
+     cElectronsRho =  ROOT.TCanvas ("cElectronsRho","cElectronsRho",750,750)
+     cElectronsRho.cd()
+     cElectronsRho.SetLogy(1)
+     BdRhoee_mass_hist.SetTitle("")
+     BdRhoee_mass_hist.GetXaxis().SetTitle("m(#pi^{-}#pi^{+}e^{+}e^{-})[MeV/c^{2}]")
+     BdRhoee_mass_hist.SetMarkerColor(17)
+     BdKstee_mass_hist.SetMarkerColor(46)
+     tBdToKstee.Draw("B_M*1000>>BdKstee_mass_hist","","")
+     tBdToRhoee.Draw("B_M*1000>>BdRhoee_mass_hist", "","SAME")
+     BdKstee_mass_hist.Scale(1./BdKstee_mass_hist.Integral())
+     BdRhoee_mass_hist.Scale(1./BdRhoee_mass_hist.Integral())
+
+     cElectronsRho.SaveAs("plots/Rhoee.png")
